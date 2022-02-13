d3ea32f3e1701ccbc0dbffa47f8e15a9a8eb74d0
Darko <darkomarjanovickop@gmail.com>
2022-02-13 21:16:18 +0100

    delete user added

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -1,4 +1,3 @@
-<div class="container-scroller">
     <div class="row p-0 m-0 proBanner" id="proBanner">
       <div class="col-md-12 p-0 m-0">
         <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
@@ -20,15 +19,15 @@
     <!-- partial:partials/_sidebar.html -->
     <nav class="sidebar sidebar-offcanvas" id="sidebar">
       <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
-        <a class="sidebar-brand brand-logo" href="index.html"><img src="assets/images/logo.svg" alt="logo" /></a>
-        <a class="sidebar-brand brand-logo-mini" href="index.html"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
+        <a class="sidebar-brand brand-logo" href="index.html"><img src="admin/assets/images/logo.svg" alt="logo" /></a>
+        <a class="sidebar-brand brand-logo-mini" href="index.html"><img src="admin/assets/images/logo-mini.svg" alt="logo" /></a>
       </div>
       <ul class="nav">
         <li class="nav-item nav-category">
           <span class="nav-link">Navigation</span>
         </li>
         <li class="nav-item menu-items">
-          <a class="nav-link" href="index.html">
+          <a class="nav-link" href="{{ url("/users") }}">
             <span class="menu-icon">
               <i class="mdi mdi-speedometer"></i>
             </span>
@@ -63,6 +62,3 @@
       
       </ul>
     </nav>
-   
-        <!-- partial -->
-      </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/", [HomeController::class, "index"]);
+
+Route::get("/users", [AdminController::class, "user"]);
+Route::get("/deleteuser/{id}", [AdminController::class, "deleteuser"]);
+
 
 Route::get("redirects", [HomeController::class, "redirects"]);
 
